8b4d8d6e36fee0a2da36035f74d731cd84e09054
Rupert Lane <rupert@rupert-lane.org>
2013-04-09 18:18:54 +0800

    Removed unused stock Ubuntu config, added README

---
+++ manifests/udon.pp
@@ -0,0 +1,5 @@
+# This is the main puppet catalog for udon. Running this will ensure
+# all items under puppet control are executed. Individual items can be
+# run by eg
+#    puppet apply -e 'include postfix'
+
