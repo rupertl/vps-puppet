c055de61624a053de4084d9b107fa79932c43f08
Rupert Lane <rupert@rupert-lane.org>
2016-04-14 12:43:30 +0700

    Updated postfix module for satellite hosts

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-This is the puppet configuration for my VPSs. Although it reflects my requirements for the servers I run, other people may find it useful so I'm putting it up on github.
+This is the puppet configuration for my VPSs and also any scratch VMs I spin up at home. Although it reflects my requirements for the servers I run, other people may find it useful so I'm putting it up on github.
 
-I'm using Debian Jessie and puppet 4.4 at present.
+This does not use a puppet master as I'm only managing a small number of nodes. Configs can be installed with `puppet apply` on each node instead.
+
+I'm using Debian Jessie and Puppet 4.4 at present. Some Puppet 4 features such as EPP templates are used. Most site specific data is managed via Hiera (plus the encrypted YAML module for sensitive data). I don't generally use modules as I only need Debian support and don't want the overhead of managing modules on each server.
 
 ## Modules
 
@@ -9,6 +11,7 @@ The configuration implements installation of the software below as modules:
 * essential: install general utility packages
 * etckeeper: store /etc in git
 * ntpd: synchronise system time
+* postfix: email server
 * ssh_public: install ssh authorized keys
 * sudo: controlled root access
 
@@ -21,7 +24,7 @@ These need to be updated
 * nginx: web server
 * nodejs: programming language
 * php: programming language
-* postfix: email server
+* postfix: email server [for mailhosts]
 * pup_check: custom script to check puppet config
 * radicale: calendar server
 
--- code/environments/production/modules/postfix/manifests/init.pp
+++ code/environments/production/modules/postfix/manifests/init.pp
@@ -1,12 +1,18 @@
 # This class manages the package, files and service for the postfix
 # email server. It only includes configs which are not stock.
 
-# Parameter server_type can be
-# "mailhost" for internet facing send/receive servers
-# "satellite" for servers that do not have local delivery and forward
-# all email to a mailhost
+# Parameters
+# server_type:
+#   "mailhost" for internet facing send/receive servers
+#   "satellite" for servers that do not have local delivery and forward
+#               all email to a mailhost
+# primary_user: who to send aliased email to
+# rewrite_from: rewritten from address for emails from satellites
+# relayhost: where to route email for satellites
+# relayuser: SASL username to use when routing satellite email
+# relaypassword: SASL username to use when routing satellite email
 
-class postfix ($server_type) {
+class postfix ($server_type, $primary_user, $rewrite_from, $relayhost, $relayuser, $relaypassword) {
   $config_dir = "/etc/postfix"
 
   if ($server_type != "mailhost" and $server_type != "satellite") {
@@ -22,23 +28,23 @@ class postfix ($server_type) {
     group => 'root',
     mode => '644',
     require => Package['postfix'],
-  }
-
-  # The config files need the service to be reloaded when changed.
-
-  file { "$config_dir/main.cf":
-    ensure => present,
-    source => "puppet:///modules/postfix/${server_type}.main.cf",
     notify => Service['postfix'],
   }
 
-  if ($server_type == "mailhost") {
-    file { "$config_dir/master.cf":
-      ensure => present,
-      source => 'puppet:///modules/postfix/master.cf',
-      notify => Service['postfix'],
-    }
+  # Postfix main config file
+  file { "$config_dir/main.cf":
+    ensure  => file,
+    content => epp("postfix/${server_type}.main.cf.epp"),
+  }
 
+  # Postfix master config file
+  file { "$config_dir/master.cf":
+    ensure  => file,
+    content => epp("postfix/${server_type}.master.cf.epp"),
+  }
+
+  # TODO: mailhost
+  if ($server_type == "mailhost") {
     file { "$config_dir/sasl/smtpd.conf":
       ensure => present,
       source => 'puppet:///modules/postfix/smtpd.conf',
@@ -53,6 +59,7 @@ class postfix ($server_type) {
     hasrestart => true,
   }
 
+  # TODO: mailhost
   if ($server_type == "mailhost") {
     # Postfix on mailhosts relies on saslauthd for authentication so
     # this is defined here also. Note that SSL certificate generation
@@ -77,27 +84,38 @@ class postfix ($server_type) {
     }
   }
 
-  # Configure SMTP forwarding to mailhost for satellities
-  # As this involves passwords this is not included here
+  # Configure SMTP forwarding to mailhost for satellites
   if ($server_type == "satellite") {
-
     file { "$config_dir/sasl_passwd":
-      replace => "no",
-      ensure  => "present",
-      content => "# This file needs to be set up\n",
-      mode    => 600,
+      ensure  => file,
+      mode => '600',
+      content => epp("postfix/sasl_passwd.epp"),
+      notify => Exec['postmap sasl_passwd'],
+    }
+
+    exec { 'postmap sasl_passwd':
+      command => "/usr/sbin/postmap hash:${config_dir}/sasl_passwd",
+      refreshonly => true,
+      notify => Service['postfix'],
     }
 
     file { "$config_dir/generic":
-      replace => "no",
-      ensure  => "present",
-      content => "# This file needs to be set up\n",
+      ensure  => file,
+      content => epp("postfix/generic.epp"),
+      notify => Exec['postmap generic'],
+    }
+
+    exec { 'postmap generic':
+      command => "/usr/sbin/postmap ${config_dir}/generic",
+      refreshonly => true,
+      notify => Service['postfix'],
     }
   }
 
   # The virtual and aliases files need to be converted to DB format
   # when changed.
 
+  # TODO: mailhost
   if ($server_type == "mailhost") {
     file { "$config_dir/virtual":
       ensure => present,
@@ -112,8 +130,8 @@ class postfix ($server_type) {
   }
 
   file { "/etc/aliases":
-    ensure => present,
-    source => "puppet:///modules/postfix/${server_type}.aliases",
+    ensure  => file,
+    content => epp("postfix/${server_type}.aliases.epp"),
     notify => Exec['newaliases'],
   }
 
@@ -121,5 +139,4 @@ class postfix ($server_type) {
     command => "/usr/bin/newaliases",
     refreshonly => true
   }
-
 }
