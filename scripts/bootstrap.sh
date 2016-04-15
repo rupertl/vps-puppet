a2e3a889fb116cd1a77dfcd585a31c70d95a3ac8
Rupert Lane <rupert@rupert-lane.org>
2016-04-15 20:23:01 +0700

    Bug fixes to bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,31 +4,33 @@
 # modules then cloning the vps-puppet repo.
 
 echo "## Updating package list"
-apt-get update
+apt-get -y update
 
 echo "## Upgrading packages"
-apt-get upgrade
+apt-get -y upgrade
 
 echo "## Installing git"
-apt-get install git sudo
+apt-get -y install git
 
 echo "## Getting Puppet PC1"
 cd /tmp
-wget https://apt.puppetlabs.com/puppetlabs-release-pc1-jessie.deb
+wget -q https://apt.puppetlabs.com/puppetlabs-release-pc1-jessie.deb
 
 echo "## Installing Puppet PC1"
 dpkg -i /tmp/puppetlabs-release-pc1-jessie.deb
 rm -f /tmp/puppetlabs-release-pc1-jessie.deb
 
 echo "## Updating package list again"
-apt-get update
+apt-get -y update
 
 echo "## Installing puppet"
-apt-get install puppet-agent
+apt-get -y install puppet-agent
+
+echo "## Moving away the old /etc/puppetlabs"
+mv /etc/puppetlabs /tmp/old-etc-puppetlabs
 
 echo "## Cloning the vps-puppet repo"
-cd /etc/puppetlabs
-git clone https://github.com/rupertl/vps-puppet.git
+git clone https://github.com/rupertl/vps-puppet.git /etc/puppetlabs
 
 echo "## All done."
 echo "## Remember to install EYAML keys to"
