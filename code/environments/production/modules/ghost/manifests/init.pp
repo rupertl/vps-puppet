a678d207964c2ca2cab12b3eec560c277e66432c
Rupert Lane <rupert@rupert-lane.org>
2016-05-15 11:18:24 +0700

    Added ghost service

---
+++ code/environments/production/modules/ghost/manifests/init.pp
@@ -0,0 +1,33 @@
+# This class manages the package, files and service for the Ghost
+# blogging service. This module only includes configs which are not
+# stock.
+
+# Parameters:
+# dir - directory where Ghost is installed
+# user - user to run ghost as
+
+class ghost(String $dir, String $user) {
+  package { ['build-essential', 'nodejs', 'nodejs-legacy', 'npm', 'unzip']:
+    ensure => installed,
+    notify => Service['ghost'],
+  }
+
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '644',
+    notify => Service['ghost'],
+  }
+
+  file { "/etc/systemd/system/ghost.service":
+    ensure  => file,
+    content => epp('ghost/ghost.service.epp'),
+  }
+
+  service { 'ghost':
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+  }
+}
